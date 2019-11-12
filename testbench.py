066dd2d4451d16dee985851f14dd528132ff2e81
Alain Lou <az2lou@uwaterloo.ca>
2019-11-12 09:31:23 -0500

    Create testbench.py

---
+++ testbench.py
@@ -0,0 +1,12 @@
+import cocotb
+from cocotb.triggers import Timer
+
+@cocotb.test()
+def dff_test(dut):
+    dut._log.info("Running dff_test")
+    for cycle in range(10):
+        dut.clk = 0
+        yield Timer(1000, units='ms')
+    	dut.clk = 1
+        yield Timer(1000, units='ms')
+    dut._log.info("Finished dff_test")
