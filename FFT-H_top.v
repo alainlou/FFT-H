61b8cee8bc378827791adddf3e183ed50282d43b
alainlou <az2lou@uwaterloo.ca>
2019-12-11 20:48:14 -0500

    WIP - change names and directory structure - assign some pins

--- FFT-H_top.v
+++
@@ -1,4 +0,0 @@
-module FFT-H_top (clk, rst, data_in, data_out);
-    input clk, rst, data_in;
-    output data_out;
-endmodule
---
+++ FFT.v
@@ -0,0 +1,4 @@
+module FFT (clk, rst, s1, l1);
+    input clk, rst, data_in;
+    output l1;
+endmodule
