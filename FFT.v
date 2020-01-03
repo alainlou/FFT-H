672df7254aa88944380d5c169d4a432213b29b30
alainlou <az2lou@uwaterloo.ca>
2020-01-02 19:22:30 -0500

    instantiate uart_rx on top level

--- FFT.v
+++ FFT.v
@@ -1,4 +1,12 @@
-module FFT (clk, rst, s1, l1);
-    input clk, rst, data_in;
-    output l1;
+module FFT (
+  input clk,
+  input rst,
+  input s1,
+  output l1);
+  
+  wire [7:0] data;
+    
+  uart_rx #(.TICKS_PER_BIT(128)) receiver (
+    clk, s1, l1, data);
+    
 endmodule
