085f34d8a850475feb6c52eb6e38ce4474c4820b
alainlou <az2lou@uwaterloo.ca>
2020-01-13 23:45:58 -0500

    add butterfly module

--- FFT.v
+++ FFT.v
@@ -4,9 +4,15 @@ module FFT (
   input uart_rxd,
   output l1);
   
-  wire [7:0] data;
+  wire [7:0] twiddle;
+  wire [7:0] data[0:1];
+  wire [7:0] transformx;
+  wire [7:0] transformy;
     
   uart_rx #(.TICKS_PER_BIT(128)) receiver (
     clk, uart_rxd, l1, data);
+  
+  butterfly b0 (
+    clk, twiddle, data[0], data[1], transform, transform);
   
 endmodule
