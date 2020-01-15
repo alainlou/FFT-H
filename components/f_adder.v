14307577759c890990de8cc6d1bee6df3e0ab27d
alainlou <az2lou@uwaterloo.ca>
2020-01-14 22:29:39 -0500

    don't need these

--- components/f_adder.v
+++
@@ -1,7 +0,0 @@
-module f_adder(n1, n2, carry_in, sum, carry_out);
-	input n1, n2, carry_in;
-	output sum, carry_out;	
-	
-	assign sum = (n1 ^ n2) ^ carry_in;
-	assign carry_out = (n1 & n2) | (carry_in	& (n1 ^ n2));
-endmodule
